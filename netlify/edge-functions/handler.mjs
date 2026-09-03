33d126407e68bb00702ce8474410b59713fb348d
julesoshaderobot <julesoshaderobot@outlook.com>
2026-09-03 18:25:07 +0800

    Enable edge function configuration and fetch handler

--- netlify/edge-functions/handler.mjs
+++ netlify/edge-functions/handler.mjs
@@ -1,5 +1,12 @@
-// export const config = { path: "/edge/*" };
+export const config = { path: "/edge/*" };
 
 // import worker from "../../src/worker.mjs";
 
 // export default worker.fetch;
+
+
+export default {
+  async fetch (request) {
+    return "";
+  }
+};
